28c28db45437399a5a318eccce60f616c4b7b09f
Chris Zhao <xinyue.zhao@immonet.de>
2017-05-17 16:36:03 +0200

    Added GET support for parameter

--- parameter.go
+++ parameter.go
@@ -12,13 +12,23 @@ type Parameter struct {
 }
 
 func NewParameter(r *http.Request) (p *Parameter) {
-	p = new(Parameter)
-	if bytes, e := ioutil.ReadAll(r.Body); e == nil {
-		if e := json.Unmarshal(bytes, p); e == nil {
-			return
+	if r.Method == "POST" {
+		p = new(Parameter)
+		if bytes, e := ioutil.ReadAll(r.Body); e == nil {
+			if e := json.Unmarshal(bytes, p); e == nil {
+				return
+			}
 		}
+		p = nil
+		return
 	}
-	p = nil
+
+	if r.Method == "GET" {
+		p = new(Parameter)
+		args := r.URL.Query()
+		p.Language = args["language"][0]
+		p.Keyword = args["keyword"][0]
+		return
+	}
 	return
 }
-
